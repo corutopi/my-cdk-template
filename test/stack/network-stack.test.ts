4768bd10011c803aeb51330f6c6dc9e410a046a5
corutopi <tekitou@tekitou>
2022-12-28 15:26:44 +0000

    Vpcテストを追加

--- test/stack/network-stack.test.ts
+++ test/stack/network-stack.test.ts
@@ -10,3 +10,15 @@ test('Snapshot', () => {
 
   expect(json).toMatchSnapshot('NetworkStackSnapshot');
 });
+
+test('Vpc', () => {
+  const app = new cdk.App();
+  const stack = new NetworkStack(app, 'TestStack');
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::EC2::VPC', 1);
+  template.hasResourceProperties('AWS::EC2::VPC', {
+    CidrBlock: '10.10.0.0/16',
+    Tags: [{ Key: 'Name', Value: 'main-vpc' }],
+  });
+});
