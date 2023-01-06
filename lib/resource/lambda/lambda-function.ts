5d0472a6b3dc3c6f929dea4704c6865470989df9
corutopi <tekitou@tekitou>
2023-01-06 17:57:03 +0000

    LambdaFunction生成ロジックをリファクタリング

--- lib/resource/lambda/lambda-function.ts
+++ lib/resource/lambda/lambda-function.ts
@@ -1,4 +1,10 @@
-import { CfnFunction, CfnAlias, CfnVersion, CfnFunctionProps } from 'aws-cdk-lib/aws-lambda';
+import {
+  CfnFunction,
+  CfnAlias,
+  CfnAliasProps,
+  CfnVersion,
+  CfnFunctionProps,
+} from 'aws-cdk-lib/aws-lambda';
 import * as cdk from 'aws-cdk-lib';
 import * as crypto from 'crypto';
 
@@ -9,6 +15,27 @@ interface LambdaFunctionProps {
   iamRole: IamRole;
 }
 
+interface CodeInfo {
+  s3Bucket: string;
+  s3Key: string;
+}
+
+interface AliasInfo {
+  name: string;
+  version: string;
+}
+
+interface ResourceInfo {
+  code: CodeInfo;
+  role: (role: IamRole) => string;
+  originName: string;
+  handler: string;
+  runtime: string;
+  aliases: AliasInfo[];
+  versionComment?: string;
+  assign: (func: LambdaFunction, cfnFunction: CfnFunction) => void;
+}
+
 /**
  * Lambda Function を生成するリソースクラス
  */
@@ -17,42 +44,88 @@ export class LambdaFunction extends BaseResource {
   public readonly SERVICE_SHORT_NAME: string = 'func';
 
   public readonly main: CfnFunction;
+
   private readonly iamRole: IamRole;
+  private readonly resourceList: ResourceInfo[] = [
+    {
+      code: {
+        s3Bucket: 'ss-test-s3',
+        s3Key: 'test-function.zip',
+      },
+      role: (iamRole) => iamRole.testFunction.attrArn,
+      originName: 'test',
+      handler: 'lambda_function.lambda_handler',
+      runtime: 'python3.8',
+      aliases: [
+        {
+          name: 'InService',
+          version: '$LATEST',
+        },
+      ],
+      versionComment: 'バージョンが変わる場合は必ずコメントも変わるものとする.',
+      assign: (func, cfnFunction) => ((func.main as CfnFunction) = cfnFunction),
+    },
+  ];
 
   constructor(parentProps: BaseProps, funcProps: LambdaFunctionProps) {
     super(parentProps);
 
     this.iamRole = funcProps.iamRole;
 
-    const fp: CfnFunctionProps = {
-      code: {
-        s3Bucket: 'ss-test-s3',
-        s3Key: 'test-function.zip',
-      },
-      role: this.iamRole.testFunction.attrArn,
-      functionName: this.createResourceName('test'),
-      handler: 'lambda_function.lambda_handler',
-      runtime: 'python3.8',
-    };
+    for (const resourceInfo of this.resourceList) {
+      resourceInfo.assign(this, this.createFunction(resourceInfo));
+    }
+  }
 
-    this.main = new CfnFunction(this.scope, this.createLogicalId('test'), fp);
-    const alias = new CfnAlias(this.scope, this.createLogicalId('test-alias'), {
-      functionName: this.main.functionName as string,
-      functionVersion: '$LATEST',
-      name: 'InService',
-    });
-    alias.addDependsOn(this.main);
-
-    const comment: string = 'バージョンが変わる場合は必ずコメントも変わるものとする.';
-    const version = new CfnVersion(
+  /**
+   * Lambda Function を生成する.
+   *
+   * @param resourceInfo - 生成するLambdaFunctionの情報を持ったインターフェース
+   * @returns 生成したLambdaFunctionインスタンス
+   */
+  private createFunction(resourceInfo: ResourceInfo): CfnFunction {
+    const cfnFunction: CfnFunction = new CfnFunction(
       this.scope,
-      this.createLogicalId('version-' + crypto.createHash('md5').update(comment).digest('hex')),
+      this.createLogicalId(resourceInfo.originName),
       {
-        functionName: this.main.functionName as string,
-        description: comment,
+        code: {
+          s3Bucket: resourceInfo.code.s3Bucket,
+          s3Key: resourceInfo.code.s3Key,
+        },
+        role: resourceInfo.role(this.iamRole),
+        functionName: this.createResourceName(resourceInfo.originName),
+        handler: resourceInfo.handler,
+        runtime: resourceInfo.runtime,
       }
     );
-    version.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
-    version.addDependsOn(this.main);
+
+    for (const aliasInfo of resourceInfo.aliases) {
+      const alias = new CfnAlias(
+        this.scope,
+        this.createLogicalId(`${resourceInfo.originName}-alias-${aliasInfo.name}`),
+        {
+          functionName: cfnFunction.functionName as string,
+          name: aliasInfo.name,
+          functionVersion: aliasInfo.version,
+        }
+      );
+      alias.addDependsOn(cfnFunction);
+    }
+
+    if (resourceInfo.versionComment !== undefined) {
+      const c = crypto.createHash('md5').update(resourceInfo.versionComment).digest('hex');
+      const version = new CfnVersion(
+        this.scope,
+        this.createLogicalId(`${resourceInfo.originName}-version-${c}`),
+        {
+          functionName: cfnFunction.functionName as string,
+          description: resourceInfo.versionComment,
+        }
+      );
+      version.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+      version.addDependsOn(cfnFunction);
+    }
+
+    return cfnFunction;
   }
 }
